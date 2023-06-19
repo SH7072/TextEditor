7ab48a8afa50c20f49789bff753476b47e278f49
Shubham Chandwani <chandwanishubham@gmail.com>
2023-06-19 15:26:22 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,62 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Editor } from '@monaco-editor/react'
+import { editor } from 'monaco-editor'
+
+const files = {
+  "temp.cpp": {
+    name: "temp.cpp",
+    language: "cpp",
+    value: "some c++ shit"
+  },
+  "index.html": {
+    name: "index.html",
+    language: "html",
+    value: "<div>hello </div>"
+  },
+  "script.py": {
+    name: "script.py",
+    language: "python",
+    value: "random code"
+
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fileName, setFileName] = useState("temp.cpp");
+  const file = files[fileName];
+  const editorRef=useRef(null);
 
+  function handleEditorDidMount(editor,monaco){
+    editorRef.current=editor;
+  }
+  function getValue(){
+    alert(editorRef.current.getValue());
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <button onClick={()=>setFileName("temp.cpp")}>Cpp</button>
+
+      <button onClick={()=>setFileName("index.html")} >  HTML </button>
+
+      <button onClick={()=>setFileName("script.py")} >Python</button>
+      <button onClick={()=>getValue( )} >getEditorValue</button>
+
+      <Editor
+        height='100vh'
+        width='600px'
+        theme='vs-dark'
+        onMount={handleEditorDidMount}
+        path={file.name}
+        defaultLanguage={file.language}
+        defaultValue={file.value}
+      />
+
+
+    </div>
+
   )
 }
 
